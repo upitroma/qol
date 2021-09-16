837b27b16b48137d6fb888a1bbe1929c434a5044
Upitroma <55750919+upitroma@users.noreply.github.com>
2021-09-16 19:47:15 -0400

    Update qol.sh

--- qol.sh
+++ qol.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-useradd -m upitroma;
-passwd upitroma;
 apt update; apt purge vim -y; apt install neovim wget curl neofetch git -y;
 curl https://github.com/upitroma.keys > /root/.ssh/authorized_keys;
-cp /root/.ssh/authorized_keys /home/upitroma/.ssh/authorized_keys;
 sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 yes/' /etc/ssh/sshd_config
 echo neofetch >> /root/.bashrc;
-echo neofetch >> /home/upitroma/.bashrc;
