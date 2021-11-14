3f4eb50ff2b4f71085730787beba5dc1b032f342
Upitroma <55750919+upitroma@users.noreply.github.com>
2021-11-14 18:17:01 -0500

    remove change to bashrc
    it was breaking scp

--- qol.sh
+++ qol.sh
@@ -4,7 +4,6 @@ apt update; apt purge vim -y; apt install neovim wget curl neofetch git openssh-
 mkdir /root/.ssh;
 curl https://github.com/upitroma.keys > /root/.ssh/authorized_keys;
 sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 yes/' /etc/ssh/sshd_config;
-echo neofetch >> /root/.bashrc;
 systemctl enable ssh;
 systemctl start ssh;
 clear;
