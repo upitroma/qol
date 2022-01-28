1ea68616a2c59ac2808a4ec188f8dbb71937260a
Upitroma <55750919+upitroma@users.noreply.github.com>
2022-01-28 15:46:51 -0500

    Update qol.sh

--- qol.sh
+++ qol.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 passwd;
-apt update; apt purge vim -y; apt install neovim wget curl neofetch git openssh-server -y;
+apt update; apt upgrade -y;
+apt purge vim -y; apt install neovim wget curl neofetch git openssh-server -y;
 mkdir /root/.ssh;
 curl https://github.com/upitroma.keys > /root/.ssh/authorized_keys;
 sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 yes/' /etc/ssh/sshd_config;
